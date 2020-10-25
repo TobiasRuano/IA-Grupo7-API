664c69dbb868852d566780f2773c976f4b2ae278
mnam7 <nammicaela@gmail.com>
2020-10-25 14:38:51 -0300

    agregado get,post y delete de recetas

---
+++ models/Receta.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+var mongoosePaginate = require('mongoose-paginate')
+
+
+var RecetaSchema = new mongoose.Schema({
+    id: String,
+    fecha: Date,
+    nombreMedico:String,
+    comentario: String
+    //imagen: de la receta
+})
+
+TurnoSchema.plugin(mongoosePaginate)
+const Receta = mongoose.model('Receta', RecetaSchema)
+
+module.exports = Receta;
