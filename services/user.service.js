399095c43b3c334379d64480aed34f34dcba5d69
Tobias Ruano <ruano.t10@gmail.com>
2020-12-18 10:46:21 -0300

    Update user.service.js

--- services/user.service.js
+++ services/user.service.js
@@ -111,7 +111,7 @@ exports.deleteUser = async function (dni) {
     // Delete the User
     try {
         var id = {dni :dni}
-        var user = await User.findOne(dni);
+        var user = await User.findOne(id);
         var deleted = await User.remove({
             dni: dni
         })
